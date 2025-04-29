1c02bf55e4f84984a89009d5275cbae90f6e765b
abiv23 <abiv23@gmail.com>
2025-04-29 10:25:22 -0600

    initial commit for todo app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { TodoFormComponent } from "./ui/todo-form.component";
+import { Todo } from "../shared/interfaces/todo";
+
+@Component({
+    selector: 'app-home',
+    template: `
+        <h2>Todo</h2>
+        <app-todo-form />
+    `,
+    imports: [TodoFormComponent]
+})
+
+export default class HomeComponent {
+    createTodo(todo: Todo) {
+        console.log(todo);
+    }
+}
---
+++ src/app/home/ui/todo-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Todo } from '../../shared/interfaces/todo';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  template: `
+    <form 
+        [formGroup]="todoForm"
+        (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())"
+    >
+      <input type="text" formControlName="title" placeholder="title..." />
+      <input
+        type="text"
+        formControlName="description"
+        placeholder="description..."
+      />
+      <button [disabled]="!todoForm.valid" type="submit">Add todo</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+export class TodoFormComponent {
+  todoSubmitted = output<Todo>()
+  private fb = inject(FormBuilder);
+
+  todoForm = this.fb.nonNullable.group({
+    title: ['', Validators.required],
+    description: [''],
+  });
+}
